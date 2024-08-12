bc776d753ec09fcf6e7606bb5a44e17f6e2c56ae
abluenautilus <abluenautilus@gmail.com>
2024-08-12 12:48:51 -0700

    each script now checks for its own subjects

--- sage_flywheel_export_bids.py
+++ sage_flywheel_export_bids.py
@@ -1,21 +1,65 @@
 #!/usr/bin/env python
 import argparse
 from subprocess import call
+import flywheel
+import os
 
 #Command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("subject", help="subject to be processed")
+# parser.add_argument("subject", help="subject to be processed")
 parser.add_argument("api_key", help="flywheel api key")
 args = parser.parse_args()
-subject_code = args.subject
+# subject_code = args.subject
 api_key = args.api_key
+
+#Set up the flywheel API
+fw = flywheel.Client(api_key)
+
+#Get the SAGE project
+project_id = "64935df4a60b3a64d6add007"
+project = fw.get(project_id)
+
+to_exclude = ["12834"]
+
+to_bidsify = []
+for subject in project.subjects():
+    session = subject.sessions()[0]
+    print("subject %s" % subject.code)
+    if 'BIDS' in session.info.keys():
+        print("BIDS already done")
+        to_bidsify.append("sub-%s" % subject.code)
+    else:
+        print("Do BIDS first")
+
+
 
 #destination for data to be saved
 dest_folder = "/Volumes/BCI/SAGE/BIDS_data/"
 
-#construct command line command
-command = "fw export bids --project \"SAGE\" --group \"teich\" --subject \"%s\" %s" %(subject_code,dest_folder)
+donefolders = os.listdir(dest_folder)
+donefolders.sort()
+to_bidsify.sort()
 
-#run it
-print(command)
-call(command,shell=True)
+numdone = len(to_bidsify)
+print("BIDS Done (%d):" % numdone)
+print(to_bidsify)
+
+numdone = len(donefolders)
+print("Already exported (%d):" % numdone)
+print(donefolders)
+
+
+to_bidsify = [elem[4:9] for elem in to_bidsify if elem not in donefolders]
+numdone = len(to_bidsify)
+print("Remaining to export (%d):" % numdone)
+print(to_bidsify)
+
+for subject_code in to_bidsify:
+
+
+    #construct command line command
+    command = "fw export bids --project \"SAGE\" --group \"teich\" --subject \"%s\" %s" %(subject_code,dest_folder)
+
+    #run it
+    print(command)
+    call(command,shell=True)
--- sage_flywheel_fixbids.py
+++ sage_flywheel_fixbids.py
@@ -4,63 +4,82 @@ import os
 import shutil
 from subprocess import call
 
-#Command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("subject", help="subject to be processed")
-args = parser.parse_args()
-subject_code = args.subject
 
-bids_folder = "/Volumes/BCI/SAGE/BIDS_data/"
-subject_folder = "%s/sub-%s" % (bids_folder,subject_code)
-inner_folder = "%s/ses-EichSAGE" % subject_folder
-fmap_folder = "%s/fmap" % inner_folder
 code_dir = os.path.dirname(os.path.realpath(__file__))
+bids_folder = "/Volumes/BCI/SAGE/BIDS_data/"
 
-if not os.path.exists(fmap_folder):
-    os.mkdir(fmap_folder)
+allsubjects = os.listdir(bids_folder)
+allsubjects.sort()
+print("Will check these:")
+print(allsubjects)
+to_do = []
 
-# Fix capitalization of anatomicals
-src_file = "%s/anat/sub-%s_ses-EichSAGE_t1w.json" % (inner_folder,subject_code)
-dest_file = "%s/anat/sub-%s_ses-EichSAGE_T1w.json" % (inner_folder,subject_code)
-if (os.path.exists(src_file)):
+for subject_code in allsubjects:
+
+    subject_folder = "%s/%s" % (bids_folder,subject_code)
+    inner_folder = "%s/ses-EichSAGE" % subject_folder
+    fmap_folder = "%s/fmap" % inner_folder
+    print("Checking %s" % fmap_folder )
+    if (not os.path.exists(fmap_folder)):
+        to_do.append(subject_code)
+
+to_do = [elem for elem in to_do if "sub-" in elem]
+print("Subjects to do:")
+print(to_do)
+
+
+for subject_code in to_do:
+    subject_folder = "%s/%s" % (bids_folder,subject_code)
+    inner_folder = "%s/ses-EichSAGE" % subject_folder
+    fmap_folder = "%s/fmap" % inner_folder
+
+    subject_code = subject_code[4:9]
+
+    if not os.path.exists(fmap_folder):
+        os.mkdir(fmap_folder)
+
+    # Fix capitalization of anatomicals
+    src_file = "%s/anat/sub-%s_ses-EichSAGE_t1w.json" % (inner_folder,subject_code)
+    dest_file = "%s/anat/sub-%s_ses-EichSAGE_T1w.json" % (inner_folder,subject_code)
+    if (os.path.exists(src_file)):
+        shutil.move(src_file,dest_file)
+
+    src_file = "%s/anat/sub-%s_ses-EichSAGE_t1w.nii.gz" % (inner_folder,subject_code)
+    dest_file = "%s/anat/sub-%s_ses-EichSAGE_T1w.nii.gz" % (inner_folder,subject_code)
+    if (os.path.exists(src_file)):
+        shutil.move(src_file,dest_file)
+
+    src_file = "%s/anat/sub-%s_ses-EichSAGE_t2w.json" % (inner_folder,subject_code)
+    dest_file = "%s/anat/sub-%s_ses-EichSAGE_T2w.json" % (inner_folder,subject_code)
+    if (os.path.exists(src_file)):
+        shutil.move(src_file,dest_file)
+
+    src_file = "%s/anat/sub-%s_ses-EichSAGE_t2w.nii.gz" % (inner_folder,subject_code)
+    dest_file = "%s/anat/sub-%s_ses-EichSAGE_T2w.nii.gz" % (inner_folder,subject_code)
+    if (os.path.exists(src_file)):
+        shutil.move(src_file,dest_file)
+
+    #Copy files to be used as field maps
+    src_file = "%s/func/sub-%s_ses-EichSAGE_task-hiAP_run-01_bold.nii.gz" % (inner_folder,subject_code)
+    dest_file = "%s/sub-%s_ses-EichSAGE_dir-1_epi.nii.gz" % (fmap_folder,subject_code)
+    shutil.copyfile(src_file,dest_file)
+
+    src_file = "%s/func/sub-%s_ses-EichSAGE_task-hiPA_run-01_bold.nii.gz" % (inner_folder,subject_code)
+    dest_file = "%s/sub-%s_ses-EichSAGE_dir-2_epi.nii.gz" % (fmap_folder,subject_code)
     shutil.move(src_file,dest_file)
 
-src_file = "%s/anat/sub-%s_ses-EichSAGE_t1w.nii.gz" % (inner_folder,subject_code)
-dest_file = "%s/anat/sub-%s_ses-EichSAGE_T1w.nii.gz" % (inner_folder,subject_code)
-if (os.path.exists(src_file)):
-    shutil.move(src_file,dest_file)
+    # Destroy the unused sidecar
+    src_file = "%s/func/sub-%s_ses-EichSAGE_task-hiPA_run-01_bold.json" % (inner_folder,subject_code)
+    os.remove(src_file)
 
-src_file = "%s/anat/sub-%s_ses-EichSAGE_t2w.json" % (inner_folder,subject_code)
-dest_file = "%s/anat/sub-%s_ses-EichSAGE_T2w.json" % (inner_folder,subject_code)
-if (os.path.exists(src_file)):
-    shutil.move(src_file,dest_file)
+    src_file = "%s/dir-1_epi.json" % code_dir
+    dest_file = "%s/sub-%s_ses-EichSAGE_dir-1_epi.json" % (fmap_folder,subject_code)
+    command = "sed 's/DEFINESUBJECT/%s/g' %s > %s" % (subject_code,src_file,dest_file)
+    print(command)
+    call(command,shell=True)
 
-src_file = "%s/anat/sub-%s_ses-EichSAGE_t2w.nii.gz" % (inner_folder,subject_code)
-dest_file = "%s/anat/sub-%s_ses-EichSAGE_T2w.nii.gz" % (inner_folder,subject_code)
-if (os.path.exists(src_file)):
-    shutil.move(src_file,dest_file)
-
-#Copy files to be used as field maps
-src_file = "%s/func/sub-%s_ses-EichSAGE_task-hiAP_run-01_bold.nii.gz" % (inner_folder,subject_code)
-dest_file = "%s/sub-%s_ses-EichSAGE_dir-1_epi.nii.gz" % (fmap_folder,subject_code)
-shutil.copyfile(src_file,dest_file)
-
-src_file = "%s/func/sub-%s_ses-EichSAGE_task-hiPA_run-01_bold.nii.gz" % (inner_folder,subject_code)
-dest_file = "%s/sub-%s_ses-EichSAGE_dir-2_epi.nii.gz" % (fmap_folder,subject_code)
-shutil.move(src_file,dest_file)
-
-# Destroy the unused sidecar
-src_file = "%s/func/sub-%s_ses-EichSAGE_task-hiPA_run-01_bold.json" % (inner_folder,subject_code)
-os.remove(src_file)
-
-src_file = "%s/dir-1_epi.json" % code_dir
-dest_file = "%s/sub-%s_ses-EichSAGE_dir-1_epi.json" % (fmap_folder,subject_code)
-command = "sed 's/DEFINESUBJECT/%s/g' %s > %s" % (subject_code,src_file,dest_file)
-print(command)
-call(command,shell=True)
-
-src_file = "%s/dir-2_epi.json" % code_dir
-dest_file = "%s/sub-%s_ses-EichSAGE_dir-2_epi.json" % (fmap_folder,subject_code)
-command = "sed 's/DEFINESUBJECT/%s/g' %s > %s" % (subject_code,src_file,dest_file)
-print(command)
-call(command,shell=True)
+    src_file = "%s/dir-2_epi.json" % code_dir
+    dest_file = "%s/sub-%s_ses-EichSAGE_dir-2_epi.json" % (fmap_folder,subject_code)
+    command = "sed 's/DEFINESUBJECT/%s/g' %s > %s" % (subject_code,src_file,dest_file)
+    print(command)
+    call(command,shell=True)
